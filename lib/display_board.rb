9f240837fe2ae71c65ef2c688b6580f620a8ee3a
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 19:33:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,4 +10,6 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
 end
 
 
-display_board(["X"," "," "," "," "," "," "," "," "])
+display_board(board)
+
+board = ["#{board[0]}","#{board[1]}","#{board[2]}","#{board[3]}","#{board[4}","#{board[5]}","#{board[6]}","#{board[7]}","#{board[8]}"]
