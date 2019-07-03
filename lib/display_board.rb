f637464c291626c1eb8a5b51bded3a64757a96c3
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 19:23:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board()
-  
-  
-  
+def display_board(board)
   
 end
+
+board = [" "," "," "," "," "," "," "," "," "]
